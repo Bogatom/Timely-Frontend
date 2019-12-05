0c20d1d0520730e36cac335a7e8ec2d24d2992b6
Tom van den Bogaart <TomvandenBogaart1@hotmail.com>
2019-12-05 12:12:25 +0100

    test fix

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,6 @@ module.exports = function(config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('karma-mocha-reporter'),
@@ -26,7 +25,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'PhantomJS'],
+    browsers: ['Chrome'],
     singleRun: false
   });
   };
