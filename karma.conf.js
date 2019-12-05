4c731164ee9fad4419b8c5eb8d0d7d9e416a76e3
Tom van den Bogaart <TomvandenBogaart1@hotmail.com>
2019-12-05 12:17:02 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['MyHeadlessChrome'],
     singleRun: false
   });
   };
